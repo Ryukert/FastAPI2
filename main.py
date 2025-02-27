6ccf616a11fc7763523d7ee531dce91c2fe5b57a
Marco Antonio <108164104+Ryukert@users.noreply.github.com>
2025-02-27 10:37:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,37 @@
 from typing import List
 from fastapi import FastAPI
-from firebase import firebase
 from pydantic import BaseModel
+import firebase_admin
+from firebase_admin import credentials, db
 
 app = FastAPI()
 
-# Configuracion de firebase
-firebaseConfig = {
-"apiKey": "AIzaSyAY41_3qeC0KAiHArv0q_nLA0YBmqd0anw",
-  "authDomain": "triodeaceleradores.firebaseapp.com",
-  "databaseURL": "https://triodeaceleradores-default-rtdb.firebaseio.com",
-  "projectId": "triodeaceleradores",
-  "storageBucket": "triodeaceleradores.firebasestorage.app",
-  "messagingSenderId": "349508741823",
-  "appId": "1:349508741823:web:eb4ca372b087a66f76740a",
-  "measurementId": "G-6BYN9N70WC"
-}
 
-# Conexion a la bd
-firebase = firebase.FirebaseApplication(firebaseConfig["databaseURL"], None)
+cred = credentials.Certificate("triodeaceleradores-firebase-adminsdk-fbsvc-6448d15405.json")  
+firebase_admin.initialize_app(cred, {
+    "databaseURL": "https://triodeaceleradores-default-rtdb.firebaseio.com"
+})
 
-# Clase para definir los datos de aceleración
+
 class Aceleracion(BaseModel):
     x: float
     y: float
     z: float
 
-# Clase para definir los datos de cada sensor
 class SensorData(BaseModel):
     sensor_id: str
     aceleracion: Aceleracion
 
-# Clase para definir los datos de múltiples sensores
 class Sensores(BaseModel):
     sensores: List[SensorData]
 
-# Añadir datos de múltiples sensores
 @app.post("/items")
 def add_items(sensores: Sensores):
+    ref = db.reference("/acelerometros/item")
     results = []
+
     for sensor in sensores.sensores:
-        result = firebase.post("/acelerometros/item", {
+        new_ref = ref.push({
             "SENSOR_ID": sensor.sensor_id,
             "ACELERACION": {
                 "X": sensor.aceleracion.x,
@@ -48,5 +39,6 @@ def add_items(sensores: Sensores):
                 "Z": sensor.aceleracion.z
             }
         })
-        results.append(result)
+        results.append({"sensor_id": sensor.sensor_id, "key": new_ref.key})
+
     return {"results": results}
